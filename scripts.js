b538999ff930761c6381561b344e5e3689618bb6
mandrei100 <mcrisandreica@hotmail.com>
2019-10-07 20:07:37 -0400

    added our starting point:

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const addRow = document.querySelector("#addRow");
+const removeRow = document.querySelector("#removeRow");
+const addColumn = document.querySelector("#addColumn");
+const removeColumn = document.querySelector("#removeColumn");
+
+const red = document.querySelector("#red");
+const blue = document.querySelector("#blue");
+const green = document.querySelector("#green");
+const yellow = document.querySelector("#yellow");
+const cyan = document.querySelector("#cyan");
+
+
