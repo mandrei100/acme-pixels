603a69ee1b268d2ddaf183ece1175b7d0df2a77b
Hector DelaRosa <hdelaros@gmail.com>
2019-10-07 21:36:37 -0400

    updates

--- scripts.js
+++ scripts.js
@@ -1,47 +1,69 @@
+const addRow = document.querySelector("#addRow");
+const removeRow = document.querySelector("#removeRow");
+const addColumn = document.querySelector("#addColumn");
+const removeColumn = document.querySelector("#removeColumn");
+
+const colorContainerChildren = [...document.querySelector('.colorContainer').children];
+const red = document.querySelector("#red");
+const blue = document.querySelector("#blue");
+const green = document.querySelector("#green");
+const yellow = document.querySelector("#yellow");
+const cyan = document.querySelector("#cyan");
+
+
 const render = (template, node) => {
     node.innerHTML += template;
 }
 
-const generateNxN = (n)=> {
-    for(let i = 0; i < n; i++){
+
+
+
+const generateNxN = (columns, rows)=> {
+    
+    for(let i = 0; i < rows; i++){
        render(`<div id="row-${i+1}"></div>`, document.querySelector('#grid'));
        const row = document.querySelector(`#row-${i+1}`);
-       for(let j = 0; j < n; j++){
-        render(`<div class="something"></div>`, row);
+       for(let j = 0; j < columns; j++){
+        render(`<div class="cells"></div>`, row);
        }
     }
 };
-const arr = generateNxN(5);
-    /* sample output
-    [
-      ['red', 'blue'],//row 1
-      ['green', '']//row 2
-    ];
-    */
+let color;
+colorContainerChildren.forEach( ele => {
+    ele.addEventListener('click', ev => {
+        colorContainerChildren.forEach( ele => {
+            ele.style.border = '1px solid black';
+        })
+        ev.target.style.border = '2px solid black';
+        color = ev.target.id;
+    })
+})
+let row = 5;
+let col = 5
+generateNxN(col,row);
 
-//const generateHTMLGrid = (arr)=> { 
-    //your code here to generate html for a grid
-  //  };
-//generateHTMLGrid(arr);
-    /*
-    <div class='row'>
-       <div style='background-color:red'></div>
-       <div style='background-color:blue'></div>
-    </div>
-    <div class='row'>
-       <div style='background-color:green'></div>
-       <div style='background-color:'></div>
-    </div>
-    */
-// const addRow = document.querySelector("#addRow");
-// const removeRow = document.querySelector("#removeRow");
-// const addColumn = document.querySelector("#addColumn");
-// const removeColumn = document.querySelector("#removeColumn");
+let cells = document.querySelectorAll('.cells');
+cells.forEach( ele => [
+    ele.addEventListener('click', ev =>{
+        ev.target.style.backgroundColor = color;
+    })
+])
 
-// const red = document.querySelector("#red");
-// const blue = document.querySelector("#blue");
-// const green = document.querySelector("#green");
-// const yellow = document.querySelector("#yellow");
-// const cyan = document.querySelector("#cyan");
+addRow.addEventListener('click', ev => {
+    //row++
+    const node = document.querySelector('#grid').lastElementChild;
+    const cloneNode = node.cloneNode(true);
+    [...cloneNode.children].forEach( ele => {
+        ele.style.backgroundColor = "";
+    })
+    document.querySelector('#grid').appendChild(cloneNode);
+    cells.document.querySelectorAll('.cells');
+
+    //generateNxN(col,row);
+})
+
+
+    
+
 
 
