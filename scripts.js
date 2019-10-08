f863d039e2d10356e29d7f5e00eca5c70c4f39b3
Hector DelaRosa <hdelaros@gmail.com>
2019-10-07 20:09:53 -0400

    scripts

---
+++ scripts.js
@@ -0,0 +1,37 @@
+const render = (template, node) => {
+    node.innerHTML += template;
+}
+
+const generateNxN = (n)=> {
+    let arr = [];
+    for(let i = 0; i < n; i++){
+        
+        for(let j = 0; i < n; j++{
+            
+        }
+    }
+return arr;
+};
+const arr = generateNxN(2);
+    /* sample output
+    [
+      ['red', 'blue'],//row 1
+      ['green', '']//row 2
+    ];
+    */
+
+//const generateHTMLGrid = (arr)=> { 
+    //your code here to generate html for a grid
+  //  };
+//generateHTMLGrid(arr);
+    /*
+    <div class='row'>
+       <div style='background-color:red'></div>
+       <div style='background-color:blue'></div>
+    </div>
+    <div class='row'>
+       <div style='background-color:green'></div>
+       <div style='background-color:'></div>
+    </div>
+    */
+console.log(arr)
